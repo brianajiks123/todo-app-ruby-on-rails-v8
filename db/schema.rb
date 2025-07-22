cc1e11bd683a4791c13fa566a219821828091647
brianajiks123 <brianajiks123@gmail.com>
2025-07-23 04:10:31 +0700

    Add connection for projects and todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_22_201547) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_22_203059) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -25,5 +25,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_22_201547) do
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false, null: false
     t.integer "priority", default: 1, null: false
+    t.integer "project_id", null: false
+    t.index ["project_id"], name: "index_todos_on_project_id"
   end
+
+  add_foreign_key "todos", "projects"
 end
