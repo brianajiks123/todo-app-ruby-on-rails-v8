bc323e363f9495e9190faa466320e81664c37fad
brianajiks123 <brianajiks123@gmail.com>
2025-07-23 01:55:19 +0700

    Add views: Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,11 @@ class ProjectsController < ApplicationController
     def index
         @projects = Project.all
     end
+
+    def show
+        # projects/id
+        # params = { "id" =>  id }
+        # params[:id]
+        @project = Project.find(params[:id])
+    end
 end
